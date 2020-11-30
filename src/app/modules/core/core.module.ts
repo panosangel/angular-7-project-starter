afe5723b2ce748cdfc09e01df69b28ff2719af96
panosangel <panagiotis@angelidakis.net>
2020-11-30 19:27:44 +0200

    Core / Imports & Providers / Enable store and http client

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,11 +1,26 @@
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import {RootEffectsModule, RootReducerModule} from '../../store/state.module';
+import {environment} from '../../../environments/environment';
+import {TodoService} from '../todo/service/todo.service';
 import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     SharedModule,
-  ]
+    HttpClientModule,
+    RootEffectsModule,
+    RootReducerModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 20,
+      logOnly: environment.production
+    })
+  ],
+  providers: [
+    TodoService,
+  ],
 })
 export class CoreModule { }
