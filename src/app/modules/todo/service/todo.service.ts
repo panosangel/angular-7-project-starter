ddf7aadaab4537a7420a7c1ab9be3b3b74f2b716
panosangel <panagiotis@angelidakis.net>
2020-11-30 19:25:56 +0200

    Todo / Service / Initialize and add getTodos method

---
+++ src/app/modules/todo/service/todo.service.ts
@@ -0,0 +1,20 @@
+import {AppState} from '../../../store/state.model';
+import {Store} from '@ngrx/store';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Todo} from '../domain/todo.model';
+
+@Injectable()
+export class TodoService {
+  baseUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  constructor(
+    private store: Store<AppState>,
+    private httpClient: HttpClient,
+  ) {
+  }
+
+  getTodos() {
+    return this.httpClient.get<Todo[]>(this.baseUrl);
+  }
+}
