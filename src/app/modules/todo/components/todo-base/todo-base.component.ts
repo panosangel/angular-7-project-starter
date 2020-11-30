d6162c7a6c226e0935ed31503fb856bcc3f9b1a5
panosangel <panagiotis@angelidakis.net>
2020-11-30 17:15:48 +0200

    Todo / Add base component

---
+++ src/app/modules/todo/components/todo-base/todo-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-base',
+  templateUrl: './todo-base.component.html',
+  styleUrls: ['./todo-base.component.css']
+})
+export class TodoBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import {TodoRoutingModule} from './todo-routing.module';
 import {SharedModule} from '../shared/shared.module';
+import { TodoBaseComponent } from './components/todo-base/todo-base.component';
 
 @NgModule({
   declarations: [
     TodoListComponent,
+    TodoBaseComponent,
   ],
   imports: [
     SharedModule,
