e742034c96f313f7bb3fbc070e0825ec6ab5955a
panosangel <panagiotis@angelidakis.net>
2020-12-01 06:52:17 +0200

    Todo / List / Implement basic functionality

--- src/app/modules/todo/components/todo-list/todo-list.component.ts
+++ src/app/modules/todo/components/todo-list/todo-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AppState} from '../../../../store/state.model';
+import {Store} from '@ngrx/store';
+
+import {GetListRequest} from '../../../../store/todo/todo.actions';
+import {selectTodoList} from '../../../../store/todo/todo.selector';
+import {Todo} from '../../domain/todo.model';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
+  private subscriptions = [];
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(
+    protected store: Store<AppState>
+  ) {
+    this.subscriptions.push(
+      store.select(selectTodoList).subscribe(todos => {
+        if (todos != null) {
+          this.todos = todos;
+        }
+      })
+    );
+  }
 
   ngOnInit() {
+    this.store.dispatch(new GetListRequest());
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }
