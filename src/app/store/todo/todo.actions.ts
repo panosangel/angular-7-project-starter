8559d9f8b7df50bb187b4697cb4769a98532ed9e
panosangel <panagiotis@angelidakis.net>
2020-12-01 09:51:53 +0200

    Store / Todo / Add DELETE_TODO_REQUEST & DELETE_TODO_RESPONSE actions

--- src/app/store/todo/todo.actions.ts
+++ src/app/store/todo/todo.actions.ts
@@ -4,6 +4,8 @@ import {Todo} from '../../modules/todo/domain/todo.model';
 
 export const GET_LIST_REQUEST = '[Todo] GetListRequest';
 export const GET_LIST_RESPONSE = '[Todo] GetListResponse';
+export const DELETE_TODO_REQUEST = '[Todo] DeleteTodoRequest';
+export const DELETE_TODO_RESPONSE = '[Todo] DeleteTodoResponse';
 export const REQUEST_FAILURE = '[Todo] RequestFailure';
 
 export class GetListRequest implements Action {
@@ -17,10 +19,23 @@ export class GetListResponse implements Action {
   }
 }
 
+export class DeleteTodoRequest implements Action {
+  readonly type = DELETE_TODO_REQUEST;
+
+  constructor(public todoId: number) {
+  }
+}
+
+export class DeleteTodoResponse implements Action {
+  readonly type = DELETE_TODO_RESPONSE;
+}
+
 export class RequestFailure implements Action {
   readonly type = REQUEST_FAILURE;
 }
 
 export type TodoActions = GetListRequest
   | GetListResponse
+  | DeleteTodoRequest
+  | DeleteTodoResponse
   | RequestFailure;
