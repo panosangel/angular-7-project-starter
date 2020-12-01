92fc1cb5945f76a00b56538eeb4d8aa2c819f162
panosangel <panagiotis@angelidakis.net>
2020-12-01 19:22:36 +0200

    State / Module / Add authReducer and AuthEffects

--- src/app/store/state.module.ts
+++ src/app/store/state.module.ts
@@ -3,13 +3,17 @@ import {StoreModule} from '@ngrx/store';
 
 import {todoReducer} from './todo/todo.reducer';
 import {TodoEffects} from './todo/todo.effects';
+import {authReducer} from './auth/auth.reducer';
+import {AuthEffects} from './auth/auth.effects';
 
 export const RootReducerModule = StoreModule.forRoot(
   {
+    auth: authReducer,
     todo: todoReducer,
   }
 );
 
 export const RootEffectsModule = EffectsModule.forRoot([
-  TodoEffects,
+  AuthEffects,
+  TodoEffects
 ]);
