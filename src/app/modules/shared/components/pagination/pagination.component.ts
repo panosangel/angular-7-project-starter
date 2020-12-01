ae7df4af61823e062456ad474d4bf5307a3dac52
panosangel <panagiotis@angelidakis.net>
2020-12-01 09:35:59 +0200

    Shared / Pagination / Implement component

--- src/app/modules/shared/components/pagination/pagination.component.ts
+++ src/app/modules/shared/components/pagination/pagination.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+
+import {Pagination} from '../../domain/pagination.model';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnChanges {
+  @Input() pagination: Pagination = new Pagination();
+  @Output() pageChanged = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    console.log(this.pagination);
+  }
+
+  hasNextPage() {
+    return this.pagination.currentPage + 1 < this.pagination.maxPage;
+  }
+
+  hasPreviousPage() {
+    return this.pagination.currentPage > 0;
+  }
+
+  buttonClicked(page: number) {
+    this.pageChanged.emit(page);
   }
 
 }
