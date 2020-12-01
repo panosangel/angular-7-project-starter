84b9f8210c7f8baf9d5f756ef4bb750d1df40d33
panosangel <panagiotis@angelidakis.net>
2020-12-01 18:13:26 +0200

    Store / Todo / Add actions GET_TODO_REQUEST & GET_TODO_RESPONSE

--- src/app/store/todo/todo.actions.ts
+++ src/app/store/todo/todo.actions.ts
@@ -4,6 +4,8 @@ import {Todo} from '../../modules/todo/domain/todo.model';
 
 export const GET_LIST_REQUEST = '[Todo] GetListRequest';
 export const GET_LIST_RESPONSE = '[Todo] GetListResponse';
+export const GET_TODO_REQUEST = '[Todo] GetTodoRequest';
+export const GET_TODO_RESPONSE = '[Todo] GetTodoResponse';
 export const DELETE_TODO_REQUEST = '[Todo] DeleteTodoRequest';
 export const DELETE_TODO_RESPONSE = '[Todo] DeleteTodoResponse';
 export const REQUEST_FAILURE = '[Todo] RequestFailure';
@@ -16,6 +18,20 @@ export class GetListResponse implements Action {
   readonly type = GET_LIST_RESPONSE;
 
   constructor(public todos: Todo[]) {
+  }
+}
+
+export class GetTodoRequest implements Action {
+  readonly type = GET_TODO_REQUEST;
+
+  constructor(public todoId: number) {
+  }
+}
+
+export class GetTodoResponse implements Action {
+  readonly type = GET_TODO_RESPONSE;
+
+  constructor(public todo: Todo) {
   }
 }
 
@@ -39,6 +55,8 @@ export class RequestFailure implements Action {
 
 export type TodoActions = GetListRequest
   | GetListResponse
+  | GetTodoRequest
+  | GetTodoResponse
   | DeleteTodoRequest
   | DeleteTodoResponse
   | RequestFailure;
