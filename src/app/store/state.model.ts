5fc225a868940c56b8fbef01ae7c17b0716935d4
panosangel <panagiotis@angelidakis.net>
2020-12-01 19:22:03 +0200

    State / Model / Add AuthState

--- src/app/store/state.model.ts
+++ src/app/store/state.model.ts
@@ -1,10 +1,15 @@
 import {Todo} from '../modules/todo/domain/todo.model';
 
 export interface AppState {
+  auth: AuthState;
   todo: TodoState;
 }
 
 export interface TodoState {
   list: Todo[];
   currentElement: Todo;
+}
+
+export interface AuthState {
+  isLoggedIn: boolean;
 }
