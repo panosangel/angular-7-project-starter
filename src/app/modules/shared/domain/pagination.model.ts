a3a8c499e453cce6e594599827dbe1a8ab7127f1
panosangel <panagiotis@angelidakis.net>
2020-12-01 08:36:01 +0200

    Shared / Domain / Add pagination model

---
+++ src/app/modules/shared/domain/pagination.model.ts
@@ -0,0 +1,21 @@
+export class Pagination {
+  pageSize = 20;
+  currentPage = 0;
+  maxPage = 0;
+  totalElements = 0;
+
+  constructor(props = {}) {
+    Object.assign(this, props);
+    this.maxPage = this.calculateMaxPage(this.totalElements, this.pageSize);
+  }
+
+ pageList() {
+    if (this.maxPage) {
+      return Array.from(Array(this.maxPage).keys());
+    }
+  }
+
+  calculateMaxPage(totalElements, pageSize) {
+    return Math.ceil(totalElements / pageSize);
+  }
+}
