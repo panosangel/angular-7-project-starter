2abb2e1c08d0ff5bb985d46ee177e2232fcf7e27
panosangel <panagiotis@angelidakis.net>
2020-12-02 07:53:38 +0200

    Shared / Main menu / Add router links

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {NgModule} from '@angular/core';
 
+import {NgModule} from '@angular/core';
 import {PaginationComponent} from './components/pagination/pagination.component';
 import {MainMenuComponent} from './components/main-menu/main-menu.component';
 import {ContactComponent} from './components/contact/contact.component';
@@ -17,6 +18,7 @@ import {ContactComponent} from './components/contact/contact.component';
     CommonModule,
     FormsModule,
     NgbModule,
+    RouterModule,
   ],
   exports: [
     CommonModule,
