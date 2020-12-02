4a4e582169165f91c3ffd4d58ecfaf9a0eb5981e
panosangel <panagiotis@angelidakis.net>
2020-12-02 16:56:41 +0200

    Todo / Routing / Add routing for edit component

--- src/app/modules/todo/todo-routing.module.ts
+++ src/app/modules/todo/todo-routing.module.ts
@@ -7,6 +7,7 @@ import {TodoBaseComponent} from './components/todo-base/todo-base.component';
 import {TodoListComponent} from './components/todo-list/todo-list.component';
 import {TodoElementResolver} from './resolver/todo-element.resolver';
 import {TodoListResolver} from './resolver/todo-list.resolver';
+import {TodoEditElementComponent} from './components/todo-edit-element/todo-edit-element.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,7 @@ const routes: Routes = [
     children: [
       {path: '', component: TodoListComponent},
       {path: 'add', component: TodoAddElementComponent},
+      {path: ':id/edit', component: TodoEditElementComponent, resolve: {todoElement: TodoElementResolver}},
       {path: ':id', component: TodoShowElementComponent, resolve: {todoElement: TodoElementResolver}},
     ]
   }
