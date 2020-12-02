cc01b6c8874af73b8cfea8217d4278edd0292c69
panosangel <panagiotis@angelidakis.net>
2020-12-02 09:11:02 +0200

    Todo / Add / Implement functionality

--- src/app/modules/todo/components/todo-add-element/todo-add-element.component.ts
+++ src/app/modules/todo/components/todo-add-element/todo-add-element.component.ts
@@ -1,4 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Store} from '@ngrx/store';
+
+import {AppState} from '../../../../store/state.model';
+import {Todo} from '../../domain/todo.model';
+import * as todoActions from '../../../../store/todo/todo.actions';
 
 @Component({
   selector: 'app-todo-add-element',
@@ -7,10 +13,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TodoAddElementComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private store: Store<AppState>,
+  ) {
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    const req = new Todo({
+      id: form.value.id,
+      userId: form.value.userId,
+      title: form.value.title,
+      checked: form.value.checked,
+    });
+    this.store.dispatch(new todoActions.AddTodoRequest(req));
   }
 
 }
